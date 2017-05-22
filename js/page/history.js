837863fc2237e373a7ca419648d43c5795f884ae
Gálffy Tamás <ezittgtx@gmail.com>
2017-05-22 20:04:57 +0300

    Added some additional data to commit tr's for later use

--- js/page/history.js
+++ js/page/history.js
@@ -22,17 +22,21 @@ $(document).ready(function() {
             let commits = data.commits;
             for(let i = 0; i < commits.length; ++i) {
                 let row = $('<tr>');
+                    row.attr('id', 'commit-row-'+commits[i]);
 
                 $('<td>')
                     .text(commits[i].substr(0, 16) + '...')
+                    .addClass('commit-hash')
                     .appendTo(row);
 
                 $('<td>')
                     .text('?')
+                    .addClass('commit-message')
                     .appendTo(row);
 
                 $('<td>')
                     .text('?')
+                    .addClass('commit-date')
                     .appendTo(row);
 
                 row.appendTo(tbody);
