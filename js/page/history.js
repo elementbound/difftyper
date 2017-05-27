966759b7c012718f83f328c4922ffd1a2b22bc29
Gálffy Tamás <ezittgtx@gmail.com>
2017-05-27 12:12:47 +0300

    Added class for querying commits one by one

--- js/page/history.js
+++ js/page/history.js
@@ -1,3 +1,51 @@
+class CommitDetailLoader {
+    constructor(repo) {
+        this.repository = repo;
+        this.commits = [];
+
+        this._to_load = [];
+
+        this.query_list();
+    }
+
+    // TODO: Make operations chainable in a nicer way
+    // ( what if we don't want to query commit data right away after the list? )
+    query_list() {
+        $.getJSON('/api/git/commits/' + this.repository, function(data) {
+            if(data.error) {
+                $(this).trigger('error');
+                return true;
+            }
+
+            this._to_load = data.commits;
+            $(this).trigger('list', [data.commits]);
+
+            $(this).trigger('progress', [0]);
+            query_next_commit();
+        });
+    }
+
+    query_next_commit() {
+        if(!this._to_load.length)
+            return false;
+
+        let hash = this._to_load.shift();
+
+        $.getJSON('/' + ['api', 'git', 'show', hash, this.repository].join('/'), function(data) {
+            if(data.error) {
+                $(this).trigger('error');
+                return true;
+            }
+
+            let commit = new Commit(data.commit);
+            this.commits.push(commit);
+
+            $(this).trigger('commit', commit);
+            $(this).trigger('progress', [this.commits.length / (this.commits.length + this._to_load.length)]);
+        });
+    }
+}
+
 $(document).ready(function() {
     /** Events **/
     $("#repo-load").click(function() {
